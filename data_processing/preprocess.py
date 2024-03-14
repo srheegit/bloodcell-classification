01d313e113d2689aab7f4e3805377cae1ec39861
kluu <134017755+luukelvin@users.noreply.github.com>
2024-03-13 19:42:52 -0700

    Add downsizing function (Sam)

--- data_processing/preprocess.py
+++ data_processing/preprocess.py
@@ -4,11 +4,13 @@ and doing initial visualization
 """
 import os
 from pathlib import Path
+import random
 
 import numpy as np
 import matplotlib.pyplot as plt
 import imageio.v3 as iio
 import cv2
+from PIL import Image
 
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -46,9 +48,95 @@ def load_images_dataset(image_dir,
                                             batch_size=batch_size,
                                             image_size=target_size)
 
-#############################
-#No longer in use:
-#############################
+# -------------------------------------
+# No longer in use:
+# -------------------------------------
+
+def downsize_images(in_dir, out_dir, target_size):
+    """
+    Downsize all images from in_dir to target_size
+    Outputs in designated out_dir directory
+
+    Parameters
+    ----------
+    in_dir: path to input directory with images
+    out_dir: path to desired output directory
+    target_size: size of reduced output images
+
+    Returns
+    -------
+    None
+    """
+    # Define the subdirectories
+    subdirectories = ["EOSINOPHIL", "LYMPHOCYTE", "MONOCYTE", "NEUTROPHIL"]
+
+    # Iterate over each subdirectory
+    for subdirectory in subdirectories:
+        # Create the output subdirectory if it does not exist
+        os.makedirs(os.path.join(out_dir, f"{subdirectory}"), exist_ok=True)
+
+        # Iterate over each file in the subdirectory
+        for filename in os.listdir(os.path.join(in_dir, subdirectory)):
+            # If the file is an image
+            if filename.endswith(".jpeg") or filename.endswith(".png"):
+                # Define the path to the image
+                input_image_path = os.path.join(in_dir, subdirectory, filename)
+                # Define the path to save the resized image
+                output_image_path = os.path.join(out_dir, f"{subdirectory}", filename)
+                # Open the original image
+                original_image = Image.open(input_image_path)
+                # Resize the image
+                resized_image = original_image.resize(target_size, Image.BICUBIC)
+                # Save the resized image
+                resized_image.save(output_image_path)
+
+    print(f"All images have been successfully resized and saved to the respective subdirectories under {out_dir}.")
+
+def load_images_limit(image_dir, num_images, labels_to_rep):
+    """
+    Loads the images at the directory path dir into an ndarray, then returns it
+    with a corresponding list of training labels (from the directory name) and
+    the names of the image files.
+
+    Parameters
+    ----------
+    image_dir: path to image directory
+    num_images: number of images to load
+    labels_to_rep: dict with keys: true labels from directory,
+                             values: corresponding integer representation
+
+    Returns
+    -------
+    images: ndarray of size (num images, 240, 320, 3)
+    images_names: list of len (num images)
+    labels: list of len (num images)
+    """
+    # create empty lists to store image data
+    images_list = [] # ndarrays for the training images
+    images_names = [] # the filename of the image
+    labels = [] # the true label for the image
+
+    # create a list to store all file paths
+    all_files = []
+
+    # loop through directory to get all file paths
+    for dirpath, dirnames, filenames in os.walk(image_dir):
+        for file in filenames:
+            all_files.append(os.path.join(dirpath, file))
+
+    # select a random subset of file paths
+    selected_files = random.sample(all_files, num_images)
+
+    # loop through the selected file paths
+    for file_path in selected_files:
+        images_list.append(iio.imread(uri = file_path).astype(np.uint8))
+        images_names.append(os.path.basename(file_path))
+        labels.append(labels_to_rep[Path(os.path.dirname(file_path)).stem])
+
+    # stack all images into one array and normalize values to [0,1]
+    images = np.stack(images_list, axis=0)/255
+
+    return images, images_names, labels
 
 def load_images_resize(image_dir, target_size, labels_to_rep):
     """
